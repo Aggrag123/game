c093f0f3cf70db009651e56dd888997998b02706
Aggrag123 <aggrahul8912@gmail.com>
2022-04-29 23:47:15 +0530

    half complted

---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        Player1 player = new Player1("Abhay", "sword", 100);
+
+        // System.out.println(player.getName());
+        // System.out.println(player.getHealth());
+        // System.out.println(player.getWeapon());
+
+
+        player.damageByGun1();
+        player.damageByGun1();
+        player.damagewByGun2();
+        player.heal();
+
+
+        // Player2 betterPlayer = new Player2("Sam", "machine gun", 80, false);
+        // betterPlayer.damageByGun1();
+
+
+
+
+    }
+    
+}
